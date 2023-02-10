23b4c5866d8a7d2301122838fff737fe4e0353fa
turtle85917 <siu85917@gmail.com>
2023-02-10 09:07:53 +0900

    Draw game board

---
+++ src/enums/Block.ts
@@ -0,0 +1,11 @@
+import { TextStyle } from "funny-terminal";
+
+export enum Block {
+  BLACK = TextStyle.DIM,
+  WHITE = TextStyle.F_WHITE,
+  RED = TextStyle.F_RED,
+  BLUE = TextStyle.F_BLUE,
+  GREEN = TextStyle.F_GREEN,
+  YELLOW = TextStyle.F_YELLOW,
+  PURPLE = TextStyle.F_MAGENTA,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-import FunnyTerminal from "funny-terminal";
+import FunnyTerminal, { putStyle } from "funny-terminal";
+import { Block } from "./enums/Block";
+
+const WIDTH = 10;
+const HEIGHT = 14;
+const BLOCK = "██";
 
 const readline = new FunnyTerminal();
 readline.setCursorShow(false);
@@ -6,6 +11,23 @@ readline.setKeypressDisable(true);
 readline.setOnlyDirectionKeys(true);
 readline.setASDWIsDirectionKeys(true);
 
-readline.addActionListener(data => {
-  readline.coverMessage(data.name)
+readline
+.addReadyListener(() => {
+  readline.coverMessage(getBoard());
+})
+.addActionListener(data => {
+  readline.coverMessage(getBoard());
 });
+
+function getBoard() {
+  let result = '';
+  for (let y = -1; y < HEIGHT+1; y++) {
+    for (let x = -1; x < WIDTH+1; x++) {
+      if (x === -1 || x === WIDTH || y === -1 || y === HEIGHT) result += putStyle(BLOCK, Block.BLACK);
+      else result += putStyle(BLOCK, Block.WHITE);
+    }
+    result += '\n';
+  }
+
+  return result;
+}
